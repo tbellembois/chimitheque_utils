317d347b1d96b64b17ceb88bdf4e03b2c4c4851d
Thomas Bellembois <thomas.bellembois@uca.fr>
2025-11-11 12:00:39 +0100

    Backup.

--- src/casbin.rs
+++ src/casbin.rs
@@ -1,3 +1,4 @@
+// Make changes in casbin.xlsx too.
 pub fn build_casbin_matchers() {
     // Request person must match policy person.
     let person_request_match = r#"(r.person_id == p.person_id)"#;
@@ -8,57 +9,38 @@ pub fn build_casbin_matchers() {
     // or if the action is write or delete the policy can be w or all
     // or if the action is all the policy must be all (redondant with the first sentence but we keep it for readability)
     let permission_equivalence_match = r#"(r.action == p.perm || (r.action == "r" && (p.perm == "w" || p.perm == "all")) || ((r.action == "w" || r.action == "d") && p.perm == "all") || (r.action == "all" && p.perm == "all"))"#;
-    // Action on bookmars or download or validate requires only permission on products (r,w,d).
-    let bookmark_match = r#"((r.item == "bookmarks" || r.item == "download" || r.item == "validate") && (p.item == "products" || p.item =="all"))"#;
-    // Can only delete products without storages.
-    let product1_match =
-        r#"(r.item == "products" && r.action == "d" && ! matchProductHasStorages(r.item_id))"#;
-    // Read or write products requires the permission on products or all.
-    let product2_match = r#"(r.item == "products" && (r.action == "r" || r.action == "w") && (p.item == "products" || p.item =="all"))"#;
-    // Request to read any (at least one) entity requires policy for at least one entity or all items.
-    // (actually everybody has this permission given that each user can read its own entity)
-    let entity1_match = r#"(r.item == "entities" && r.action == "r" && r.item_id == "" && (p.item == "entities" || p.item =="all"))"#;
-    // Request to read a specific entity requires to be member of the entity.
-    let entity2_match = r#"(r.item == "entities" && r.action == "r" && r.item_id != "" && matchPersonIsInEntity(r.person_id, r.item_id))"#;
-    // Request to create an entity only for admins
-    let entity3_match = r#"(r.item == "entities" && r.action == "w" && r.item_id == "" && (p.perm == "all" && p.item == "all" && p.entity_id == "-1"))"#;
-    // Request to delete a specitic entity requires to have policy to write all entites (admin) - and entity must have no members and no store locations.
-    let entity4_match = r#"(r.item == "entities" && r.action == "d" && r.item_id != "" && (p.perm == "all" && p.item == "all" && p.entity_id == "-1") && ! matchEntityHasMembers(r.item_id) && ! matchEntityHasStoreLocations(r.item_id))"#;
-    // Request to write a specitic entity requires to have policy to write the entity or all.
-    let entity5_match = r#"(r.item == "entities" && r.action == "w" && r.item_id == p.entity_id && (p.item == "entities" || p.item =="all") && matchPersonIsInEntity(r.person_id, r.item_id))"#;
-    // Request to read/write/delete any (at least one) storage requires policy for at least one storage or all.
-    let storage1_match = r#"((r.item == "storages" || r.item == "stocks" || r.item == "borrows") && r.item_id == "" && (p.item == "storages" || p.item =="all"))"#;
-    // Request to read/write/delete a specific storage requires to be member of the same entity as the storage store location.
-    let storage2_match = r#"((r.item == "storages" || r.item == "stocks" || r.item == "borrows") && r.item_id != "" && matchPersonIsInStorageEntity(r.person_id, r.item_id, p.entity_id))"#;
-    // Request to read any (at least one) store location requires policy of at least one storage or all.
-    let store_location1_match = r#"(r.item == "store_locations" && r.action == "r" && r.item_id == "" && (p.item == "storages" || p.item =="all"))"#;
-    // Request to read a specific store location requires policy for storage and to be member of the same entity as the store location
-    let store_location2_match = r#"(r.item == "store_locations" && r.action == "r" && r.item_id != "" && matchPersonIsInStoreLocationEntity(r.person_id, r.item_id, p.entity_id))"#;
-    // Request to delete any (at least one) store location requires policy of at least one entity or all - and store location must have no children and storages.
-    let store_location3_match = r#"(r.item == "store_locations" && r.action == "d" && r.item_id != "" && (p.item == "entities" || p.item =="all") && ! matchStoreLocationHasChildren(r.item_id) && ! matchStoreLocationHasStorages(r.item_id))"#;
-    // Request to write any (at least one) store location requires policy of at least one entity or all.
-    let store_location4_match = r#"(r.item == "store_locations" && (r.action == "w" || r.action == "d") && r.item_id == "" && (p.item == "entities" || p.item =="all"))"#;
-    // Request to write/delete a specific store location requires policy for entity and to be member of the same entity as the store location.
-    let store_location5_match = r#"(r.item == "store_locations" && (r.action == "w" || r.action == "d") && r.item_id != "" && matchPersonIsInStoreLocationEntity(r.person_id, r.item_id, p.entity_id))"#;
-    // Request to read any (at least one) person requires policy of at least one entity or all.
-    let people1_match = r#"(r.item == "people" && r.action == "r" && r.item_id == "" && (p.item == "entities" || p.item =="all"))"#;
-    // Request to read a specific person (except oneself) requires policy for entity and to be member of the same entity as the person.
-    let people2_match = r#"(r.item == "people" && r.action == "r" && r.item_id != p.person_id && r.item_id != "" && matchPersonIsInPersonEntity(r.person_id, r.item_id, p.entity_id))"#;
-    // Can not delete oneself oan admin or a manager.
-    let people3_match = r#"(r.item == "people" && r.action == "d" && r.item_id != "" && (p.item == "entities" || p.item =="all") && r.item_id != p.person_id && ! matchPersonIsAdmin(r.person_id) && ! matchPersonIsManager(r.person_id))"#;
-    // Request to write/delete any (at least one) person requires policy of at least one entity or all.
-    let people4_match = r#"(r.item == "people" && (r.action == "w" || r.action == "d") && r.item_id == "" && (p.item == "entities" || p.item =="all"))"#;
-    // -> Request to write/delete a specific person (except oneself) requires policy for entity and to be member of the same entity as the person
-    let people5_match = r#"(r.item == "people" && (r.action == "w" || r.action == "d") && r.item_id != p.person_id && r.item_id != "" && matchPersonIsInPersonEntity(r.person_id, r.item_id))"#;
-    // No restriction to retrieve user information.
-    let userinfo_match = r#"(r.item == "userinfo")"#;
-    // No restriction to ping the server.
-    let ping_match = r#"(r.item == "ping")"#;
-
-    let match_items = format!("( {bookmark_match} || {product1_match} || {product2_match} || {entity1_match} || {entity2_match} || {entity3_match} || {entity4_match} || {entity5_match} || {storage1_match} || {storage2_match} || {store_location1_match} || {store_location2_match} || {store_location3_match} || {store_location4_match} || {store_location5_match} ||  {people1_match} || {people2_match} || {people3_match} || {people4_match} || {people5_match} )");
+    // Permissions definition.
+    let rules_match = r#"\
+    ((r.item == "products" && r.action == "c") && p.perm == "w" && (p.item == "products" || p.item =="all")) || \
+    ((r.item == "products" && r.action == "r" && r.item_id == "") && p.perm == "r" && (p.item == "products" || p.item =="all")) || \
+    ((r.item == "products" && r.action == "r" && r.item_id != "") && p.perm == "r" && (p.item == "products" || p.item =="all")) || \
+    ((r.item == "products" && r.action == "u") && p.perm == "w" && (p.item == "products" || p.item =="all")) || \
+    ((r.item == "products" && r.action == "d") && p.perm == "w" && (p.item == "products" || p.item =="all")) || \
+    ((r.item == "storages" && r.action == "c") && p.perm == "w" && (p.item == "storages" || p.item =="all")) || \
+    ((r.item == "storages" && r.action == "r" && r.item_id == "") && p.perm == "r" && (p.item == "storages" || p.item =="all")) || \
+    ((r.item == "storages" && r.action == "r" && r.item_id != "") && p.perm == "r" && (p.item == "storages" || p.item =="all") && matchPersonAndStorageAreInEntity({r.person_id},{r.item_id},{p.entity_id})) || \
+    ((r.item == "storages" && r.action == "u") && p.perm == "w" && (p.item == "storages" || p.item =="all") && matchPersonAndStorageAreInEntity({r.person_id},{r.item_id},{p.entity_id})) || \
+    ((r.item == "storages" && r.action == "d") && p.perm == "w" && (p.item == "storages" || p.item =="all") && matchPersonAndStorageAreInEntity({r.person_id},{r.item_id},{p.entity_id})) || \
+    ((r.item == "store_locations" && r.action == "c") && p.perm == "w" && (p.item == "entities" || p.item =="all")) || \
+    ((r.item == "store_locations" && r.action == "r" && r.item_id == "") && p.perm == "r" && (p.item == "entities" || p.item =="all")) || \
+    ((r.item == "store_locations" && r.action == "r" && r.item_id != "") && p.perm == "r" && (p.item == "entities" || p.item =="all") && matchPersonAndStoreLocationAreInEntity({r.person_id},{r.item_id},{p.entity_id})) || \
+    ((r.item == "store_locations" && r.action == "u") && p.perm == "w" && (p.item == "entities" || p.item =="all") && matchPersonAndStoreLocationAreInEntity({r.person_id},{r.item_id},{p.entity_id})) || \
+    ((r.item == "store_locations" && r.action == "d") && p.perm == "w" && (p.item == "entities" || p.item =="all") && matchPersonAndStoreLocationAreInEntity({r.person_id},{r.item_id},{p.entity_id}) && !matchStoreLocationHasChildren({r.item_id}) && !matchStoreLocationHasStorages({r.item_id})) || \
+    ((r.item == "people" && r.action == "c") && p.perm == "w" && (p.item == "entities" || p.item =="all")) || \
+    ((r.item == "people" && r.action == "r" && r.item_id == "") && p.perm == "r" && (p.item == "entities" || p.item =="all")) || \
+    ((r.item == "people" && r.action == "r" && r.item_id != "") && p.perm == "r" && (p.item == "entities" || p.item =="all") && matchPersonAndPersonAreInEntity({p.entity_id},{r.item_id})) || \
+    ((r.item == "people" && r.action == "u") && p.perm == "w" && (p.item == "entities" || p.item =="all") && matchPersonAndPersonAreInEntity({p.entity_id},{r.item_id})) || \
+    ((r.item == "people" && r.action == "d") && p.perm == "w" && (p.item == "entities" || p.item =="all") && matchPersonAndPersonAreInEntity({p.entity_id},{r.item_id}) && !matchPersonIsManager({r.item_id})&& !matchPersonIsadmin({r.item_id})) || \
+    ((r.item == "entities" && r.action == "c") && p.perm == "w" && p.entity_id == -1 && p.item == all) || \
+    ((r.item == "entities" && r.action == "r" && r.item_id == "") && p.perm == "r") || \
+    ((r.item == "entities" && r.action == "r" && r.item_id != "") && p.perm == "r" && r.item_id == p.entity_id) || \
+    ((r.item == "entities" && r.action == "u") && p.perm == "w" && r.item_id == p.entity_id) || \
+    ((r.item == "entities" && r.action == "d") && p.perm == "w" && p.entity_id == -1 && p.item == all && !matchEntityHasMembers({r.item_id}) && !matchEntityHasStoreLocations({r.item_id})) || \
+    ((r.item == bookmarks" || r.item == "download" || r.item == "validate") && (p.item == "products" || p.item =="all")) || \
+    ((r.item == userinfo" || r.item == "ping"))"#;
 
     println!(
-        "m = ( {person_request_match} && ( {is_admin_match} || {permission_equivalence_match} ) && ( {userinfo_match} || {ping_match} || {match_items} ) )"
+        "m = ( {person_request_match} && ( {is_admin_match} || {permission_equivalence_match} ) && ( {rules_match} ) )"
     );
     // println!("m = ( {product1_match} || {product2_match} )");
 }
